c0c923dd9ab4b24e2a71dd00ae96b053ec60848b
Ronald <rslj0225@gmail.com>
2018-01-05 18:54:19 -0500

    Update Twigs

--- src/AppBundle/Controller/Products_CategoryController.php
+++ src/AppBundle/Controller/Products_CategoryController.php
@@ -133,4 +133,134 @@ class Products_CategoryController extends Controller
             ->getForm()
         ;
     }
+
+    /**
+     * Displays a form to edit an existing product entity.
+     *
+     * @Route("/list/cat/ajax", name="categorias_show_ajax")
+     * @Method("GET")
+     */
+
+    public function getProductsAjax(Request $request)
+    {
+        ini_set('max_execution_time',0);
+
+        $f_i = $request->query->get('fecha_i');
+        $f_f = $request->query->get('fecha_f');
+    
+        $sEcho = $request->query->get('draw');
+        $iDisplayStart = $request->query->get('start');
+        $iDisplayLength = $request->query->get('length');
+
+        //Ordering
+        $iSortCol_0 = $request->query->get('iSortCol_0');
+        $iSortingCols = $request->query->get('iSortingCols');
+        $aColumns = array("c.id", "c.code", "c.name", "c.description", "c.active");
+        
+        $sWhere = '';
+        $OrderD = 'ASC';
+        //Searching
+        $sSearch = $request->query->get('search')['value'];        
+        $OrderD = $request->query->get('order')[0]['dir'];
+
+        //Ordering
+        $sByColumn = $request->query->get('order')[0]['column'];
+
+        $bY = "c.id";
+        if($sByColumn == 0){
+
+            $bY="c.id";
+
+        }elseif($sByColumn == 1){
+
+            $bY="c.code";
+
+        }elseif($sByColumn == 2){
+
+            $bY="c.name";
+
+        }elseif($sByColumn == 3){
+
+            $bY="c.description";
+
+        }elseif($sByColumn == 4){
+
+            $bY="c.active";
+
+        }       
+        
+        if ($sSearch != null && $sSearch != "")
+        {
+            if ($sWhere == '') {
+                $sWhere .= 'WHERE (';
+            } else {
+                $sWhere .= 'AND (';
+            }
+
+            for ($i = 0; $i < count($aColumns); $i++) {
+                $sWhere .= $aColumns[$i] . ' LIKE "%' . $sSearch . '%" OR ';
+            }
+
+            $sWhere = substr_replace($sWhere, '', -3);
+            $sWhere .= ')';
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            "SELECT c.id, c.code, c.name, c.description, c.active
+            FROM AppBundle:Products_Category c "
+            . $sWhere . 
+            " ORDER BY "
+            . $bY . " " . $OrderD
+        );
+
+        $inventario = $query->setMaxResults($iDisplayLength)
+                            ->setFirstResult($iDisplayStart)
+                            ->getResult();
+
+
+        $query = $em->createQuery(
+            "SELECT c.id, c.code, c.name, c.description, c.active
+            FROM AppBundle:Products_Category c "
+            . $sWhere . 
+            " ORDER BY "
+            . $bY . " " . $OrderD
+        );
+
+        $inventario2 = $query->getResult();
+
+        $filteredInventario = count($inventario);
+        $totalInventario    = count($inventario2);
+
+        $output = array(
+            "draw"            => $sEcho,
+            "recordsTotal"    => $filteredInventario,
+            "recordsFiltered" => $totalInventario,
+            "data"            => array(),
+        );
+       
+        foreach ($inventario as $inv)
+        {
+            $options    =   '<a class="btn btn-success" 
+                            href="/products_category/'.$inv['id'].'/edit">
+                                <i class="fa fa-edit"></i> Edit</a>'
+                            .' <a class="btn btn-success" href="/products_category/'.$inv['id'].'">
+                            <i class="fa fa-edit"></i> Ver</a>';
+
+            $row = array();          
+
+            $row[] = $inv['id'];
+            $row[] = $inv['code'];
+            $row[] = $inv['name'];
+            $row[] = $inv['description'];
+            $row[] = $inv['active'];
+            $row[] = $options;
+
+            $output['data'][] = $row;
+
+        }
+
+        return $this->json($output);
+    }
 }
